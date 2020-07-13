92adb8c699e8c9a7d910b3ecc9ac8c88c319f98e
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-07-13 10:33:39 -0400

    possible

--- pool/models.py
+++ pool/models.py
@@ -32,15 +32,14 @@ class Group(BaseGroup):
         total_payoff = sum(contributions)
         self.total_contribution = 11*(self.total_payoff) - (1/16)*(self.total_payoff)^2
         for p in players:
-            set_ratio(p)
+            set_ratio(p.sent_tokens)
             p.payoff = p.private_tokens+((p.ratio)*self.total_contribution)
 
 
 
 class Player(BasePlayer):
     ratio = models.CurrencyField()
-    payoff = models.CurrencyField()
     sent_tokens = models.CurrencyField(min = 0, max = Constants.endowment, label = "Pool Tokens")
     private_tokens = models.CurrencyField(min = 0, max = Constants.endowment, label = "Private Tokens")
-    def set_ratio(Player):
+    def set_ratio(Player.sent_tokens):
         self.ratio = self.sent_tokens / Group.total_payoff
