b1b71f983d4b80047a10fb4fca02b19d55f71e47
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-07-23 11:26:21 -0400

    possible

--- voter/models.py
+++ voter/models.py
@@ -43,66 +43,73 @@ class Group(BaseGroup):
         elif player1.voter_decision1() < player2.voter_decision1():
             votes2.append(1)
         elif player1.voter_decision1() == player2.voter_decision1():
+            self.winner = 'Tie'
             choice = R.randint(0, 1)
             if choice == 0:
                 votes1.append(1)
             if choice == 1:
                 votes2.append(1)
-        if player1.voter_decision1() > player2.voter_decision1():
+        if player1.voter_decision2() > player2.voter_decision2():
             votes1.append(1)
-        elif player1.voter_decision1() < player2.voter_decision1():
+        elif player1.voter_decision2() < player2.voter_decision2():
             votes2.append(1)
-        elif player1.voter_decision1() == player2.voter_decision1():
+        elif player1.voter_decision2() == player2.voter_decision2():
+            self.winner = 'Tie'
             choice = R.randint(0, 1)
             if choice == 0:
                 votes1.append(1)
             if choice == 1:
                 votes2.append(1)
-        if player1.voter_decision1() > player2.voter_decision1():
+        if player1.voter_decision3() > player2.voter_decision3():
             votes1.append(1)
-        elif player1.voter_decision1() < player2.voter_decision1():
+        elif player1.voter_decision3() < player2.voter_decision3():
             votes2.append(1)
-        elif player1.voter_decision1() == player2.voter_decision1():
+        elif player1.voter_decision3() == player2.voter_decision3():
+            self.winner = 'Tie'
             choice = R.randint(0, 1)
             if choice == 0:
                 votes1.append(1)
             if choice == 1:
                 votes2.append(1)
-        if player1.voter_decision1() > player2.voter_decision1():
+        if player1.voter_decision4() > player2.voter_decision4():
             votes1.append(1)
-        elif player1.voter_decision1() < player2.voter_decision1():
+        elif player1.voter_decision4() < player2.voter_decision4():
             votes2.append(1)
-        elif player1.voter_decision1() == player2.voter_decision1():
+        elif player1.voter_decision4() == player2.voter_decision4():
+            self.winner = 'Tie'
             choice = R.randint(0, 1)
             if choice == 0:
                 votes1.append(1)
             if choice == 1:
                 votes2.append(1)
-        if player1.voter_decision1() > player2.voter_decision1():
+        if player1.voter_decision5() > player2.voter_decision5():
             votes1.append(1)
-        elif player1.voter_decision1() < player2.voter_decision1():
+        elif player1.voter_decision5() < player2.voter_decision5():
             votes2.append(1)
-        elif player1.voter_decision1() == player2.voter_decision1():
+        elif player1.voter_decision5() == player2.voter_decision5():
+            self.winner = 'Tie'
             choice = R.randint(0, 1)
             if choice == 0:
                 votes1.append(1)
             if choice == 1:
                 votes2.append(1)
-        if player1.voter_decision1() > player2.voter_decision1():
+        if player1.voter_decision6() > player2.voter_decision6():
             votes1.append(1)
-        elif player1.voter_decision1() < player2.voter_decision1():
+        elif player1.voter_decision6() < player2.voter_decision6():
             votes2.append(1)
-        elif player1.voter_decision1() == player2.voter_decision1():
+        elif player1.voter_decision6() == player2.voter_decision6():
+            self.winner = 'Tie'
             choice = R.randint(0, 1)
             if choice == 0:
                 votes1.append(1)
             if choice == 1:
                 votes2.append(1)
-        if player1.voter_decision1() > player2.voter_decision1():
+        if player1.voter_decision7() > player2.voter_decision7():
             votes1.append(1)
-        elif player1.voter_decision1() < player2.voter_decision1():
+        elif player1.voter_decision7() < player2.voter_decision7():
             votes2.append(1)
-        elif player1.voter_decision1() == player2.voter_decision1():
+        elif player1.voter_decision7() == player2.voter_decision7():
+            self.winner = 'Tie'
             choice = R.randint(0, 1)
             if choice == 0:
                 votes1.append(1)
@@ -118,37 +125,25 @@ class Group(BaseGroup):
             players[0].winner = True
             players[1].payoff = -100
             players[0].payoff = 100
-        if players[1].votes == players[0].votes:
-            self.winner = 'Tie'
-            choice = R.randint(0, 1)
-            if choice == 0:
-                players[1].winner = True
-                players[1].payoff = 100
-                players[0].payoff = -100
-            if choice == 1:
-                players[0].winner = True
-                players[1].payoff = -100
-                players[0].payoff = 100
+
 
 class Player(BasePlayer):
     winner = models.BooleanField(initial = False)
     votes = models.IntegerField()
     opponent_platform = models.FloatField()
     platform = models.FloatField(label = "Your Platform: ", min = 0, max = 1)
-    def vars_for_template(self):
-        a = 7 - self.votes
-        return dict(a = a)
+
     def voter_decision1(self):
-        return(10-10*abs(self.platform - .1))
+        return(10-(10*abs(self.platform - .1)))
     def voter_decision2(self):
-        return(10-10*abs(self.platform - .25))
+        return(10-(10*abs(self.platform - .25)))
     def voter_decision3(self):
-        return(10-10*abs(self.platform - .35))
+        return(10-(10*abs(self.platform - .35)))
     def voter_decision4(self):
-        return(10-10*abs(self.platform - .4))
+        return(10-(10*abs(self.platform - .4)))
     def voter_decision5(self):
-        return(10-10*abs(self.platform - .5))
+        return(10-(10*abs(self.platform - .5)))
     def voter_decision6(self):
-        return(10-10*abs(self.platform - .75))
+        return(10-(10*abs(self.platform - .75)))
     def voter_decision7(self):
-        return(10-10*abs(self.platform - .85))
+        return(10-(10*abs(self.platform - .85)))
