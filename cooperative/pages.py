33c389c05cc0d3d64af6b794017128496d5a12cb
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-07-28 09:06:14 -0400

    possible

--- cooperative/pages.py
+++ cooperative/pages.py
@@ -6,7 +6,7 @@ from .models import Constants
 class Introduction(Page):
     pass
 
-class Main(page):
+class Main(Page):
     form_model = 'player'
     form_fields = ['give', 'keep']
 
@@ -14,7 +14,7 @@ class Main(page):
         print('value is', values)
         if((values['give'] + values['keep']) != 100):
             return "Total ticket distribution must total 100 tickets"
-    
+
     live_method = 'live_bid'
 
 
