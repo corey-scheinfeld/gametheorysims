642073f70b7f3a0540459e99bde9784d4989ed22
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-07-29 10:21:22 -0400

    possible

--- cooperative/models.py
+++ cooperative/models.py
@@ -25,8 +25,8 @@ class Subsession(BaseSubsession):
 
 class Group(BaseGroup):
     def live_auction(self, id_in_group, data):
-        print('working')
         print('Player two offered a lottery ticket split of ' + str(data[0])+'/'+str(data[1])+', meaning player two would keep '+str(data[0])+' tickets, and that you would recieve '+str(data[1])+' tickets.')
+        return {0: 'thanks'}
 
 
 class Player(BasePlayer):
