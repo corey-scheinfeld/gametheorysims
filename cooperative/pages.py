0c6b95b624eb8b16b9989f55814e3ce4121b49a2
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-07-30 10:35:00 -0400

    possible

--- cooperative/pages.py
+++ cooperative/pages.py
@@ -17,18 +17,14 @@ class auction(Page):
     def get_timeout_seconds(self):
         return self.player.my_page_timeout_seconds
 
-    def is_displayed(self):
-        if self.player.offer_accepted == True:
-            return False
-        else:
-            return True
+
     def before_next_page(self):
         players = self.player.get_others_in_group()
         for p in players:
             p.give = self.player.keep
             p.keep = self.player.give
             p.my_page_timeout_seconds = 0
-            self.get_timeout_seconds()
+            get_timeout_seconds(self.p)
 
 class ResultsWaitPage(WaitPage):
     def after_all_players_arrive(self):
