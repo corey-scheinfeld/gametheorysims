52c934d9cb91d426ef67958d1a0b129b091f4395
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-07-30 09:49:40 -0400

    possible

--- cooperative/pages.py
+++ cooperative/pages.py
@@ -23,7 +23,7 @@ class auction(Page):
             p.give = self.player.keep
             p.keep = self.player.give
             p.my_page_timeout_seconds = 0
-            p.get_timeout_seconds()
+            self.get_timeout_seconds()
 
 class ResultsWaitPage(WaitPage):
     def after_all_players_arrive(self):
