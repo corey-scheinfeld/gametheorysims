c24de0c2d812b756147367170f8e6d33ad289a85
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-08-04 18:52:40 -0400

    possible

--- coalition/models.py
+++ coalition/models.py
@@ -39,7 +39,7 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    contract = models.StringField(initial='None')
+    contract = models.StringField(initial=string('None'))
     firmA = models.IntegerField(label = "Firm A Merger Profit:", blank=True)
     firmB = models.IntegerField(label = "Firm B Merger Profit:", blank=True)
     firmC = models.IntegerField(label= "Firm C Merger Profit:", blank=True)
