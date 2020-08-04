717769a1ca4e8191acfa95d646076a9aadcb9730
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-08-04 17:06:53 -0400

    possible

--- coalition/models.py
+++ coalition/models.py
@@ -34,7 +34,7 @@ class Group(BaseGroup):
     finished = models.IntegerField(initial = 0)
     def live_agreement(self, id_in_group, data):
         if(data == 'game_finished'):
-            self.group.finished = int(self.group.finished) + 1
+            self.finished = int(self.finished) + 1
             return{0: int(self.group.finished)}
 
 
