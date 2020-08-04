fcd95adfae2fb484c01ef11dcea378f57695386c
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-08-04 12:15:15 -0400

    possible

--- cooperative/models.py
+++ cooperative/models.py
@@ -37,8 +37,8 @@ class Group(BaseGroup):
     def live_auction(self, id_in_group, data):
         if data == 'game_finished':
             return {0: data}
-        if(int(data[0])+int(data[1])>100):
-            return{id_in_group: data}
+        if(int(data[0])+int(data[1]) != 100):
+            return{id_in_group: 'error'}
         elif id_in_group == 1:
             self.get_player_by_id(1).keep = int(data[0])
             self.get_player_by_id(1).give = int(data[1])
