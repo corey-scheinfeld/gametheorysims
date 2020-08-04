ff667c992294b0db4485f4367a5da10895d8ee6e
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-08-04 15:11:42 -0400

    possible

--- coalition/pages.py
+++ coalition/pages.py
@@ -41,28 +41,6 @@ class ResultsWaitPage(WaitPage):
 
 
 class Results(Page):
-    def vars_for_template(self):
-        if(self.player.role() == 'A'):
-            if(self.player.contract == 'A and B'):
-                a = 'B'
-            if(self.player.contract == 'A and C'):
-                a = 'C'
-            if(self.player.contract == 'A, B and C'):
-                a = 'B and C'
-        if(self.player.role() == 'B'):
-            if(self.player.contract == 'A and B'):
-                a = 'A'
-            if(self.player.contract == 'B and C'):
-                a = 'C'
-            if(self.player.contract == 'A, B and C'):
-                a = 'A and C'
-        if(self.player.role() == 'C'):
-            if(self.player.contract == 'A and C'):
-                a = 'A'
-            if(self.player.contract == 'B and C'):
-                a = 'B'
-            if(self.player.contract == 'A, B and C'):
-                a = 'A and C'
-        return dict(a=a)
+    pass
 
 page_sequence = [Introduction, Main, Contract, ResultsWaitPage, Results]
