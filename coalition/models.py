7309fc8dbe36715116c6e3ce11dda3d4838d50b5
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-08-05 10:19:35 -0400

    possible

--- coalition/models.py
+++ coalition/models.py
@@ -49,7 +49,7 @@ class Group(BaseGroup):
             players = self.get_player_by_id(id_in_group).get_others_in_group()
             for p in players:
                 if p.merged == True:
-                    if p.firmA == int(data['firmA']) and p.firmB == int(data['firmB']) and p.firmC == in(data['firmC']):
+                    if p.firmA == int(data['firmA']) and p.firmB == int(data['firmB']) and p.firmC == int(data['firmC']):
                         self.get_player_by_id(id_in_group).firmA = int(data['firmA'])
                         self.get_player_by_id(id_in_group).firmB = int(data['firmB'])
                         self.get_player_by_id(id_in_group).firmC = int(data['firmC'])
