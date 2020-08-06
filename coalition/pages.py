a193ef4842059e3904a6ba84bfc352871b0456ff
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-08-06 12:09:13 -0400

    possible

--- coalition/pages.py
+++ coalition/pages.py
@@ -31,7 +31,7 @@ class Contract(Page):
         if self.player.contract == None:
             return False
         else:
-            return True
+            return (True and (self.get_timeout_seconds() > 3))
     form_model = 'player'
     def get_form_fields(self):
         if self.player.contract == 'A and B':
@@ -60,11 +60,21 @@ class Contract(Page):
     timer_text = 'Time left to complete this section:'
     def get_timeout_seconds(self):
         return self.participant.vars['expiry'] - time.time()
-    def is_displayed(self):
-        return self.get_timeout_seconds() > 3
+    def before_next_page(self):
+        self.player.complete = True
+        players = self.player.get_others_in_group()
+        for p in players:
+            if (p.merged == True and p.complete == True):
+
+                if ((p.firmA == self.player.firmA) and (p.firmB == self.player.firmB) and (p.firmC == self.player.firmC)):
+                    self.group.matching_contract = True
+                else:
+                    self.group.matching_contract = False
+                    self.group.chances = self.group.chances+1
 
 
 class second_chance(Page):
+
     form_model = 'player'
     def get_form_fields(self):
         if self.player.contract == 'A and B':
@@ -76,25 +86,13 @@ class second_chance(Page):
         if self.player.contract == 'A, B and C':
             return ['firmA', 'firmB', 'firmC']
         return []
-    def is_displayed(self):
-        return ((self.group.chances == 1) and (not(self.group.matching_contract)) and (self.player.merged == True))
+
     timer_text = 'Time left to complete this section:'
     def get_timeout_seconds(self):
         return self.participant.vars['expiry'] - time.time()
     def is_displayed(self):
-        return self.get_timeout_seconds() > 3
-    def before_next_page(self):
-        self.player.complete = True
-        players = self.player.get_others_in_group()
-        for p in players:
-            if (p.merged == True and p.complete == True):
-                log((p.firmA == self.player.firmA) and (p.firmB == self.player.firmB) and (p.firmC == self.player.firmC))
-                if ((p.firmA == self.player.firmA) and (p.firmB == self.player.firmB) and (p.firmC == self.player.firmC)):
-                    self.group.matching_contract = True
-                else:
-                    self.group.matching_contract = False
-                    self.group.chances = self.group.chances+1
-
+        log((p.firmA == self.player.firmA) and (p.firmB == self.player.firmB) and (p.firmC == self.player.firmC))
+        return ((self.group.chances == 1) and (not(self.group.matching_contract)) and (self.player.merged == True) and (self.get_timeout_seconds() > 3))
 
 class ResultsWaitPage(WaitPage):
     pass
