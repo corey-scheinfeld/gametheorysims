047288f2fe55e491093767656adbe7f366c264b7
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-08-07 09:53:38 -0400

    possible

--- coalition/pages.py
+++ coalition/pages.py
@@ -62,7 +62,6 @@ class Contract(Page):
         players = self.player.get_others_in_group()
         for p in players:
             if (p.merged == True and p.complete == True):
-
                 if ((p.firmA == self.player.firmA) and (p.firmB == self.player.firmB) and (p.firmC == self.player.firmC)):
                     self.group.matching_contract = True
                 else:
@@ -94,7 +93,7 @@ class second_chance(Page):
     def get_timeout_seconds(self):
         return self.participant.vars['expiry'] - time.time()
     def is_displayed(self):
-        return ((self.group.chances == 1) and (not(self.group.matching_contract)) and (self.player.merged == True) and (self.get_timeout_seconds() > 3))
+        return ((self.group.chances == 2) and (not(self.group.matching_contract)) and (self.player.merged == True) and (self.get_timeout_seconds() > 3))
 
 class ResultsWaitPage(WaitPage):
     title_text = "Contract Finalization"
