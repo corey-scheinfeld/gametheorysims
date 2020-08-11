105a902246d2274fddf6e562fb736529f4b187e2
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-08-11 10:55:05 -0400

    possible

--- condorcet/models.py
+++ condorcet/models.py
@@ -2,7 +2,7 @@ from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
- import Random as R
+import Random as R
 
 author = 'Your name here'
 
