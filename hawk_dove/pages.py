0bbdca1c1f4e1d7ee6aa332fe429f1f904659369
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-08-18 11:33:27 -0400

    possible

--- hawk_dove/pages.py
+++ hawk_dove/pages.py
@@ -2,7 +2,7 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
-class Introduction(page):
+class Introduction(Page):
     def is_displayed(self):
         def is_displayed(self):
             return self.round_number==1
