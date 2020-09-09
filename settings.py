e8aa7c1cffe37822f536a9c14c27a91735095972
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-09-08 20:01:41 -0400

    possible

--- settings.py
+++ settings.py
@@ -221,7 +221,7 @@ USE_POINTS = False
 ROOMS = [
     {
         'name': 'class',
-        'display_name': 'ECON280: Game Theory '
+        'display_name': 'ECON280: Game Theory ',
         'participant_label_file':'_rooms/gametheory_discussion.txt'
     }
 ]
