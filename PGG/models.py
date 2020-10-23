b641814da45214e307b5d48e61a579a398e2b98d
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-10-22 20:39:03 -0400

    changes

--- PGG/models.py
+++ PGG/models.py
@@ -26,12 +26,14 @@ class Constants(BaseConstants):
     players_per_group = 3
     num_rounds = 10
 
+    instructions_reg = 'PGG/instructions_reg.html'
+    instructions_pun = 'PGG/instructions_pun.html'
 
 class Subsession(BaseSubsession):
     def creating_session(self):
         if self.round_number == 1:
             num_players = 1
-            rand_val = itertools.cycle(['pun-partisan', 'pun-partisan', 'pun-partisan', 'pun-partisan', 'reg_partisan', 'reg_partisan', 'reg_partisan', 'reg_partisan', 'pun_control', 'reg_control'])
+            rand_val = itertools.cycle(['pun_partisan', 'pun_partisan', 'pun_partisan', 'pun_partisan', 'reg_partisan', 'reg_partisan', 'reg_partisan', 'reg_partisan', 'pun_control', 'reg_control'])
             for p in self.get_players():
                 if(num_players%3 == 0):
                     p.participant.vars['role'] = next(rand_val)
@@ -48,9 +50,16 @@ class Subsession(BaseSubsession):
 
 class Group(BaseGroup):
     type = models.StringField()
+    group_pot = models.IntegerField()
     playerA = models.StringField()
     playerB = models.StringField()
     playerC = models.StringField()
+    A_cont = models.IntegerField()
+    B_cont = models.IntegerField()
+    C_cont = models.IntegerField()
+    A_payoff = models.IntegerField()
+    B_payoff = models.IntegerField()
+    C_payoff = models.IntegerField()
     rand_val = models.IntegerField()
     def adjust_group(self):
         labels = ['A', 'B', 'C']
@@ -64,14 +73,31 @@ class Group(BaseGroup):
         self.playerA = players[0].affiliation
         self.playerB = players[1].affiliation
         self.playerC = players[2].affiliation
-
+    def set_pot(self):
+        self.group_pot = 0
+        for players in self.get_players():
+            self.group_pot += players.group_contribution
+            players.kept = 20 - players.group_contribution
+        for players in self.get_players():
+            players.individual_share = self.group_pot*(2/3)
+            players.payoff = 20 - players.group_contribution + players.individual_share
+        players = self.get_players()
+        self.A_cont = players[0].group_contribution
+        self.B_cont = players[1].group_contribution
+        self.C_payoff = players[2].group_contribution
+        self.A_payoff = players[0].payoff
+        self.B_payoff = players[1].payoff
+        self.C_payoff = players[2].payoff
 
 
 class Player(BasePlayer):
     affiliation = models.StringField(choices = ['Democrat', 'Republican'], widget=widgets.RadioSelect)
     label = models.StringField()
-    group_type = models.StringField()
-    group_bonus = models.IntegerField()
-    personal_bonus = models.IntegerField()
+    group_contribution = models.IntegerField(label = "Your Contribution to the Group Project:", min = 0, max = 20)
+    individual_share = models.IntegerField()
+    kept = models.IntegerField()
     choice = models.StringField(
     choices = ['Participate', 'Do Not Participate'])
+    punishA = models.IntegerField(min = 0, max = 5)
+    punishB = models.IntegerField(min = 0, max = 5)
+    punishC = models.IntegerField(min = 0, max = 5)
--- participation/pages.py
+++ participation/pages.py
@@ -13,24 +13,37 @@ class IntroductionWaitPage(WaitPage):
     after_all_players_arrive = 'set_up'
 
 
-class Main(Page):
+class contribution(Page):
     form_model = 'player'
-    form_fields = ['choice']
-
-    def vars_for_template(self):
-        a = self.round_number - 5
-        return dict(a = a)
+    form_fields = ['group_contribution']
 
 
 class ResultsWaitPage(WaitPage):
-    wait_for_all_groups = True
-    after_all_players_arrive = 'set_payoffs'
+    body_text = """After all group members have arrived, we calculate\n total_contributions = sum(contribution) for all players\n individual_share = 2/3 total_contributions\n\n
+    stage 1 payoffs (for each individual) = 20 - contribution + individual_share"""
+    after_all_players_arrive = 'set_pot'
 
 
-
-
-class Results(Page):
+class Results1(Page):
     pass
+
+class punishment_part(Page):
+    def is_displayed(self):
+        return self.group.type == 'pun_partisan'
+
+    form_model = 'player'
+    def get_form_fields(self):
+        if self.group.type == 'pun_partisan' || self.group.type == 'pun_control':
+            if self.player.label == 'A':
+                return ['punishB', 'punishC']
+            if self.player.label == 'B':
+                return ['punishA', 'punishC']
+            if self.player.label == 'C':
+                return ['punishA', 'punishB']
+        else:
+            return []
+
+
 
 
 page_sequence = [
