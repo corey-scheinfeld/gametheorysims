0019a823e26ffdc045357091c98276fcf4e0a4aa
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-10-22 20:45:12 -0400

    changes

--- PGG/pages.py
+++ PGG/pages.py
@@ -24,16 +24,34 @@ class GroupWaitPage(WaitPage):
 class group_display(Page):
     pass
 
-class MyPage(Page):
-    pass
+class contribution(Page):
+    form_model = 'player'
+    form_fields = ['group_contribution']
 
 
 class ResultsWaitPage(WaitPage):
+    body_text = """After all group members have arrived, we calculate\n total_contributions = sum(contribution) for all players\n individual_share = 2/3 total_contributions\n\n
+    stage 1 payoffs (for each individual) = 20 - contribution + individual_share"""
+    after_all_players_arrive = 'set_pot'
+
+
+class Results1(Page):
     pass
 
+class punishment_part(Page):
+    def is_displayed(self):
+        return self.group.type == 'pun_partisan'
 
-class Results(Page):
-    pass
+    form_model = 'player'
+    def get_form_fields(self):
+        if self.group.type == 'pun_partisan' or self.group.type == 'pun_control':
+            if self.player.label == 'A':
+                return ['punishB', 'punishC']
+            if self.player.label == 'B':
+                return ['punishA', 'punishC']
+            if self.player.label == 'C':
+                return ['punishA', 'punishB']
+        else:
+            return []
 
-
-page_sequence = [MyWaitPage, partisan, GroupWaitPage, group_display]
+page_sequence = [MyWaitPage, partisan, GroupWaitPage, group_display, ResultsWaitPage, Results1, punishment_part]
--- participation/pages.py
+++ participation/pages.py
@@ -13,37 +13,24 @@ class IntroductionWaitPage(WaitPage):
     after_all_players_arrive = 'set_up'
 
 
-class contribution(Page):
+class Main(Page):
     form_model = 'player'
-    form_fields = ['group_contribution']
+    form_fields = ['choice']
+
+    def vars_for_template(self):
+        a = self.round_number - 5
+        return dict(a = a)
 
 
 class ResultsWaitPage(WaitPage):
-    body_text = """After all group members have arrived, we calculate\n total_contributions = sum(contribution) for all players\n individual_share = 2/3 total_contributions\n\n
-    stage 1 payoffs (for each individual) = 20 - contribution + individual_share"""
-    after_all_players_arrive = 'set_pot'
+    wait_for_all_groups = True
+    after_all_players_arrive = 'set_payoffs'
 
 
-class Results1(Page):
+
+
+class Results(Page):
     pass
-
-class punishment_part(Page):
-    def is_displayed(self):
-        return self.group.type == 'pun_partisan'
-
-    form_model = 'player'
-    def get_form_fields(self):
-        if self.group.type == 'pun_partisan' or self.group.type == 'pun_control':
-            if self.player.label == 'A':
-                return ['punishB', 'punishC']
-            if self.player.label == 'B':
-                return ['punishA', 'punishC']
-            if self.player.label == 'C':
-                return ['punishA', 'punishB']
-        else:
-            return []
-
-
 
 
 page_sequence = [
