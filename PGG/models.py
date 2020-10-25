f3627687c579c6efb9393fc776330aa5597baabd
corey-scheinfeld <60146627+corey-scheinfeld@users.noreply.github.com>
2020-10-25 19:52:04 -0400

    my commit message

--- PGG/models.py
+++ PGG/models.py
@@ -128,9 +128,9 @@ class Group(BaseGroup):
                 if self.type == 'pun_control' or self.type == 'pun_partisan':
                     for i in range(1, self.subsession.round_number + 1):
                         player.final_payoff = player.in_round(i).round_payoff + player.final_payoff
-                    else:
-                        for i in range(1, self.subsession.round_number + 1):
-                            player.final_payoff = int(player.in_round(i).first_payoff) + player.final_payoff
+                else:
+                    for i in range(1, self.subsession.round_number + 1):
+                        player.final_payoff = int(player.in_round(i).first_payoff) + player.final_payoff
     def set_final_payoff(self):
         for player in self.get_players():
             if self.type == 'pun_control' or self.type == 'pun_partisan':
