80161f40e951bfd3bf3cf4646224d9f3abb955c2
corey-scheinfeld <cscheinfeld@middlebury.edu>
2021-02-16 20:34:19 -0500

    pennies_fix

--- pennies/models.py
+++ pennies/models.py
@@ -20,6 +20,7 @@ class Subsession(BaseSubsession):
     stage = models.IntegerField()
     def creating_session(self):
         if self.round_number == 1:
+            self.stage  = 1
             self.group_randomly()
             for p in self.get_players():
                 p.participant.vars['total'] = 0
