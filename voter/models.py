0fd541c02cfec9a740661525b5bc0f81701b8366
corey-scheinfeld <cscheinfeld@middlebury.edu>
2021-02-16 21:49:40 -0500

    pennies_fix

--- voter/models.py
+++ voter/models.py
@@ -40,13 +40,27 @@ class Group(BaseGroup):
         ideal = []
         players[0].opponent_platform = players[1].platform
         players[1].opponent_platform = players[0].platform
-        for(x in range(0, 7)):
+        if(players[1].platform == players[0].platform){
+            self.winner = 'Tie'
+            return
+        }
+        for x in range(0, 7):
             plat1 = (10-(10*abs(player1.platform - platforms[x])))
             plat2 = (10-(10*abs(player2.platform - platforms[x])))
             if(plat1 > plat2):
                 votes1.append(1)
+                ideal.append(1)
             elif (plat2 > plat1):
                 votes2.append(1)
+                ideal.append(2)
+            elif plat1 == plat2:
+                choice = R.randint(0, 1)
+                if choice == 0:
+                    votes1.append(1)
+                    ideal.append(1)
+                if choice == 1:
+                    votes2.append(1)
+                    ideal.append(2)
         players[0].votes = len(votes1)
         players[1].votes = len(votes2)
         if players[1].votes > players[0].votes:
@@ -57,17 +71,6 @@ class Group(BaseGroup):
             players[0].winner = True
             players[1].payoff = -100
             players[0].payoff = 100
-        elif player[1].votes == player[0].votes:
-            self.winner = "Tie"
-            choice = R.randint(0, 1)
-            if choice == 0:
-                players[0].winner = True
-                players[1].payoff = -100
-                players[0].payoff = 100
-            if choice == 1:
-                players[1].winner = True
-                players[1].payoff = 100
-                players[0].payoff = -100
 
 
 class Player(BasePlayer):
