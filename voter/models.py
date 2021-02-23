22375dd97e7da98d2453da932b7ddaa07dc9e043
corey-scheinfeld <cscheinfeld@middlebury.edu>
2021-02-23 16:13:52 -0500

    spring_labels

--- voter/models.py
+++ voter/models.py
@@ -37,29 +37,26 @@ class Group(BaseGroup):
         votes1 = []
         votes2 = []
         platforms = [.1, .25, .35, .4, .5, .75, .85]
-        ideal = []
         players[0].opponent_platform = players[1].platform
         players[1].opponent_platform = players[0].platform
         if(players[1].platform == players[0].platform):
             self.winner = 'Tie'
+            players[0].votes = 0
+            players[1].votes = 0
             return
         for x in range(0, 7):
             plat1 = (10-(10*abs(player1.platform - platforms[x])))
             plat2 = (10-(10*abs(player2.platform - platforms[x])))
             if(plat1 > plat2):
-                votes1.append(1)
-                ideal.append(1)
+                votes1.append(x)
             elif (plat2 > plat1):
-                votes2.append(1)
-                ideal.append(2)
+                votes2.append(x)
             elif plat1 == plat2:
                 choice = R.randint(0, 1)
                 if choice == 0:
-                    votes1.append(1)
-                    ideal.append(1)
+                    votes1.append(x)
                 if choice == 1:
-                    votes2.append(1)
-                    ideal.append(2)
+                    votes2.append(x)
         players[0].votes = len(votes1)
         players[1].votes = len(votes2)
         if players[1].votes > players[0].votes:
