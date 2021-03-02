77c0bdae5b8d44b3388ea1708b4c0d9ea536d741
corey-scheinfeld <cscheinfeld@middlebury.edu>
2021-03-02 17:29:55 -0500

    voterfix

--- voter/models.py
+++ voter/models.py
@@ -28,44 +28,46 @@ class Subsession(BaseSubsession):
         self.group_randomly()
 
 class Group(BaseGroup):
-    winner = models.StringField()
-
+    Tie = models.BooleanField()
 
     def set_payoffs(self):
-        players = self.get_players()
+        players = self.get_players(initial = False)
         player1 = players[0]
         player2 = players[1]
         platforms = [.1, .25, .35, .4, .5, .75, .85]
-        players[0].opponent_platform = players[1].platform
-        players[1].opponent_platform = players[0].platform
-        if(players[1].platform == players[0].platform):
-            self.winner = 'Tie'
-            players[0].votes = 0
-            players[1].votes = 0
-            return
+        player1.opponent_platform = player2.platform
+        player2.opponent_platform = player1.platform
+        player1.participant.vars['votes'] = []
+        player2.participant.vars['votes'] = []
+        if(player1.platform == player2.platform):
+            self.Tie = True
         for x in range(0, 7):
             plat1 = (10-(10*abs(player1.platform - platforms[x])))
             plat2 = (10-(10*abs(player2.platform - platforms[x])))
             if(plat1 > plat2):
-                votes1.append(x)
+                player1.participant.vars['votes'].append('W')
+                player2.participant.vars['votes'].append('L')
             elif (plat2 > plat1):
-                votes2.append(x)
+                player1.participant.vars['votes'].append('L')
+                player2.participant.vars['votes'].append('W')
             elif plat1 == plat2:
                 choice = R.randint(0, 1)
                 if choice == 0:
-                    votes1.append(x)
+                    player1.participant.vars['votes'].append('TW')
+                    player2.participant.vars['votes'].append('TL')
                 if choice == 1:
-                    votes2.append(x)
-        players[0].votes = len(votes1)
-        players[1].votes = len(votes2)
-        if players[1].votes > players[0].votes:
-            players[1].winner = True
-            players[1].payoff = 100
-            players[0].payoff = -100
-        if players[1].votes < players[0].votes:
-            players[0].winner = True
-            players[1].payoff = -100
-            players[0].payoff = 100
+                    player1.participant.vars['votes'].append('TL')
+                    player2.participant.vars['votes'].append('TW')
+        player1.votes = len(player1.participant.vars['votes'])
+        player2.votes = len(player2.participant.vars['votes'])
+        if player1.votes > player2.votes:
+            player1.winner = True
+            player1.payoff = 100
+            player2.payoff = -100
+        if player1.votes < player2.votes:
+            player2.winner = True
+            player2.payoff = 100
+            player1.payoff = -100
 
 
 class Player(BasePlayer):
