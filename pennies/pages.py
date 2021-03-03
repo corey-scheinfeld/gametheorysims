99fe5da5c13857d234890f19b750f474a2f4be73
corey-scheinfeld <cscheinfeld@middlebury.edu>
2021-03-02 21:25:08 -0500

    pennyfix

--- pennies/pages.py
+++ pennies/pages.py
@@ -12,9 +12,14 @@ class Main(Page):
     form_fields = ['choice']
 
     def vars_for_template(self):
-        return {
-            'round': self.round_number % 4
-        }
+
+        if(self.subsession.round_number %4 != 0):
+            return {
+                'round': self.subsession.round_number % 4
+                }
+        else:
+            return 4
+
 
 
 class ResultsWaitPage(WaitPage):
@@ -29,11 +34,16 @@ class Results(Page):
 
     def vars_for_template(self):
         opponent = self.player.other_player()
+        r = 0
+        if(self.subsession.round_number %4 != 0):
+            r = self.subsession.round_number %4
+        else:
+            r = 4
         return {
             'player_payoff': int(self.player.payoff),
             'opponent_choice': opponent.choice,
             'stage': (self.round_number // 4) + 1,
-            'round': self.round_number % 4
+            'round': r
 
         }
 
